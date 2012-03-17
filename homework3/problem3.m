5ec12f2a8815f6bbe5f74dd137bf2c46d8b6d2d8
Your Name <martyfuhry@gmail.com>
2012-03-17 19:35:58 -0400

    got both of these working. just need to print out some graphs at different times and i'm all set.

--- homework3/problem3.m
+++ homework3/problem3.m
@@ -26,28 +26,46 @@ function [f] = f(u)
     f = 0.5*u.^2;
 endfunction
 
+function [m] = minf(u_l, u_r)
+    tol = (u_r - u_l) / 10.;
+    if abs(tol) <= 10e-16;
+        m = f(u_l);
+    else
+        u = [u_l:tol:u_r];
+        m = min(f(u));
+    endif
+endfunction
+
+function [m] = maxf(u_l, u_r)
+    tol = (u_r - u_l) / 10.;
+    if abs(tol) <= 10e-16;
+        m = f(u_l);
+    else
+        u = [u_l:tol:u_r];
+        m = max(f(u));
+    endif
+endfunction
+
 % set up some PDE variables
-m = 50;
+m = 100;
 a = -1;
 b = 1;
 
 % discretize the grid
 dx = (b - a) / (m - 1);
-x = [a-dx:dx:b+dx]';
-
-% keep the courant number 1 so that there is no dispersion
-dt = 0.5*dx;
+x = [a:dx:b]';
+dt = dx;
 timesteps = 1/dt; 
-global mu = dt/dx;
+mu = dt/dx;
 
 u_lefts  = [0, 1, -1, 1]; 
 u_rights = [1, 0, 1, -1];
 for k = 1:4
     u_l = u_lefts(k);
     u_r = u_rights(k);
-    u = zeros(m+2,1);
+    u = zeros(m,1);
 
-    for j = 1:m+2
+    for j = 1:m
         if x(j) <= 0
             u(j) = u_l;
         else
@@ -59,43 +77,64 @@ for k = 1:4
     u_lw = u;
     u_g  = u;
 
-    % start the FD method
+    % start the FV method
     for t = 1:ceil(timesteps)
         % inflow conditions
         u_lf(1) = u_l;
         u_lw(1) = u_l;
+        u_g(1)  = u_l;
 
         %%%%%%%%%%%%%%%%%%%%%%%
-        % Lax-Friedrichs
+        % Lax-Friedrichs      %
         %%%%%%%%%%%%%%%%%%%%%%%
-        F = [0; f([u_lf(3:m+2)]) - f([u_lf(1:m)]); 0];
-        A = diag(ones(m+1,1),-1) + diag(ones(m+1,1),1);
-        A(m+2, m+1) = 2; % need to get outflow right.
+        % calculate the flux
+        flux = f(u_lf);
+        F = diag(ones(m-1,1),1) - diag(ones(m-1,1),-1);
+        F(1,1) = -1;
+        F(m,m) = 1;
+        A = diag(ones(m-1,1),-1) + diag(ones(m-1,1),1);
+        A(1, 1)   = 1; % this should be fine
+        A(m, m-1) = 2; % need to get outflow right
 
-        u_lf = 0.5*A*u_lf - 0.5*mu*F;
+        u_lf = 0.5*A*u_lf - 0.5*mu*F*flux;
 
         %%%%%%%%%%%%%%%%%%%%%%%
-        % Lax-Wendroff
+        % Lax-Wendroff        %
         %%%%%%%%%%%%%%%%%%%%%%%
-        F_left  = [0; f(u_lw(2:m+2)) - f(u_lw(1:m+1))];
-        F_right = [f(u_lw(2:m+2)) - f(u_lw(1:m+1)); 0];
-        A_right = diag(ones(m+2,1)) + diag(ones(m+1,1),1);
-        A_left  = diag(ones(m+2,1)) + diag(ones(m+1,1),-1);
-        A_left(1,1)      = 2; % need to get outflow right.
-        A_right(m+2,m+2) = 2; % need to get outflow right.
-        ustar_left  = 0.5*A_left *u_lw - 0.5*mu*F_left;
-        ustar_right = 0.5*A_right*u_lw - 0.5*mu*F_right;
+        % calculate the flux
+        flux = f(u_lw);
+        F_left  = diag(ones(m,1)) - diag(ones(m-1,1),-1);
+        F_right = diag(ones(m-1,1),1) - diag(ones(m,1));
+        F_left(1,1)  = 0; % already have inflow 
+        F_right(m,m) = 0; % this should be fine
+        A_right = diag(ones(m,1)) + diag(ones(m-1,1),1);
+        A_left  = diag(ones(m,1)) + diag(ones(m-1,1),-1);
+        A_left(1,1)  = 2; % already have inflow
+        A_right(m,m) = 2; % need to get outflow right
+        ustar_left  = 0.5*A_left *u_lw - 0.5*mu*F_left*flux;
+        ustar_right = 0.5*A_right*u_lw - 0.5*mu*F_right*flux;
 
         u_lw = u_lw - mu*(f(ustar_right) - f(ustar_left));
         
-
         %%%%%%%%%%%%%%%%%%%%%%%
-        % Godunov
+        % Godunov             %
         %%%%%%%%%%%%%%%%%%%%%%%
+        F = zeros(m,1);
+        F(1) = f(u_l); % inflow
+        % calculate flux
+        for i = 2:m
+            if u_g(i-1) <= u_g(i)
+                F(i) = minf(u_g(i-1), u_g(i));   
+            else
+                F(i) = maxf(u_g(i-1), u_g(i));
+            endif
+         endfor
+        u_g = u_g - mu*([F(2:m); F(m)] - F);
 
         hold on
         plot(x,u_lf, 'b.-');
         plot(x,u_lw, 'r-'); 
+        plot(x,u_g, '--'); 
         axis([a,b,-1.5,1.5])
         drawnow
         clf;
---
+++ homework3/problem4.m
@@ -0,0 +1,222 @@
+% Problem 4
+% Marty Fuhry
+% 3/17/2011
+% Compiled and ran using GNU Octave, version 3.2.4 configured for "x86_64-pc-linux-gnu".
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Write a finite volume code for linear advection with constant a=1            %
+%  u_t + a u_x = 0                                                             %
+% and compare the performance of upwinding and Lax-Wendroff                    %
+%  F = au_j-1^n + a/2 (1-mu)(u_j^n - u_j-1^n)                                  %
+% using flux limiters minmod                                                   %
+%  max(0, min(1, r))                                                           %
+% superbee                                                                     %
+%  max(0, min(1, 2r), min(2, r))                                               %
+% and Van Leer                                                                 %
+%  (r + abs(r))/(1 + abs(r))                                                   %
+% How do these compare (see paper)?                                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Octave is dumb; tell it this is not a function file
+1;
+
+function [C] = minmod(r)
+    m = size(r)(1);
+    C = zeros(m,1);
+    for k = 1:m
+        C(k) = max(0, min(1,r(k)));
+    endfor
+endfunction
+
+function [C] = superbee(r)
+    m = size(r)(1);
+    C = zeros(m,1);
+    for k = 1:m
+        C(k) = max([0, min(1,2*r(k)), min(2,r(k))]);
+    endfor
+endfunction
+
+function [C] = vanleer(r)
+    C = (r + abs(r))./(1 + abs(r));
+endfunction
+
+% set up some PDE variables
+m = 100;
+a = -1;
+b = 1;
+
+% discretize the grid
+dx = (b - a) / (m - 1);
+x = [a:dx:b]';
+dt = 0.5*dx;
+timesteps = 1/dt; 
+mu = dt/dx;
+
+% initial conditions
+u = zeros(m,1);
+u_l = 2;
+u_r = 0;
+for j = 1:m
+    if x(j) <= 0
+        u(j) = u_l;
+    else
+        u(j) = u_r;
+    endif
+endfor
+
+% set each seperate methods initial conditions
+uminmod   = u;
+usuperbee = u;
+uvanleer  = u;
+uupwind   = u;
+uexact    = u;
+
+% set left flux matrices
+A_left = diag(ones(m,1)) - diag(ones(m-1,1),-1);
+A_left(1,1) = 0;
+B_left = diag(ones(m-1,1), -1);
+B_left(1,1) = 1;
+
+% set right flux matrices
+A_right = diag(ones(m-1,1),1) - diag(ones(m,1));
+A_right(m,m) = 0;
+B_right = diag(ones(m,1));
+
+A = diag(ones(m-1,1),-1) - diag(ones(m-2,1),-2);
+B = diag(ones(m,1)) - diag(ones(m-1,1),-1);
+
+% start the FV method
+for t = 1:timesteps
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % minmod                                     %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % get ratios          %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        r1 = A*uminmod;
+        r2 = B*uminmod;
+        r2(r2 == 0) = 1e-10;
+        r = r1./r2;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % flux calculation    %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        Fl_low  = B_left*uminmod;
+        Fl_high = B_left*uminmod + 0.5*(1 - mu)*A_left*uminmod;
+
+        Fr_low  = B_right*uminmod;
+        Fr_high = B_right*uminmod + 0.5*(1 - mu)*A_right*uminmod;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % update u            %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        C = minmod(r);
+        F_left  = Fl_low + C.*(Fl_high - Fl_low);
+        F_right = Fr_low + C.*(Fr_high - Fr_low);
+        F = F_right - F_left;
+        uminmod = uminmod - mu*F;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % superbee                                   %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % get ratios          %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        r1 = A*usuperbee;
+        r2 = B*usuperbee;
+        r2(r2 == 0) = 1e-10;
+        r = r1./r2;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % flux calculation    %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        Fl_low  = B_left*usuperbee;
+        Fl_high = B_left*usuperbee + 0.5*(1 - mu)*A_left*usuperbee;
+
+        Fr_low  = B_right*usuperbee;
+        Fr_high = B_right*usuperbee + 0.5*(1 - mu)*A_right*usuperbee;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % update u            %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        C = superbee(r);
+        F_left  = Fl_low + C.*(Fl_high - Fl_low);
+        F_right = Fr_low + C.*(Fr_high - Fr_low);
+        F = F_right - F_left;
+        usuperbee = usuperbee - mu*F;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % van leer                                   %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % get ratios          %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        r1 = A*uvanleer;
+        r2 = B*uvanleer;
+        r2(r2 == 0) = 1e-10;
+        r = r1./r2;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % flux calculation    %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        Fl_low  = B_left*uvanleer;
+        Fl_high = B_left*uvanleer + 0.5*(1 - mu)*A_left*uvanleer;
+
+        Fr_low  = B_right*uvanleer;
+        Fr_high = B_right*uvanleer + 0.5*(1 - mu)*A_right*uvanleer;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % update u            %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        C = vanleer(r);
+        F_left  = Fl_low + C.*(Fl_high - Fl_low);
+        F_right = Fr_low + C.*(Fr_high - Fr_low);
+        F = F_right - F_left;
+        uvanleer = uvanleer - mu*F;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % upwinding                                  %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % flux calculation    %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        Fl_low  = B_left*uupwind;
+        Fl_high = B_left*uupwind + 0.5*(1 - mu)*A_left*uupwind;
+
+        Fr_low  = B_right*uupwind;
+        Fr_high = B_right*uupwind + 0.5*(1 - mu)*A_right*uupwind;
+
+        %%%%%%%%%%%%%%%%%%%%%%%
+        % update u            %
+        %%%%%%%%%%%%%%%%%%%%%%%
+        C = zeros(size(r));
+        F_left  = Fl_low + C.*(Fl_high - Fl_low);
+        F_right = Fr_low + C.*(Fr_high - Fr_low);
+        F = F_right - F_left;
+        uupwind = uupwind - mu*F;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % exact solution                             %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    for j = 1:m
+        if x(j) <= dt*t
+            uexact(j) = u_l;
+        else
+            uexact(j) = u_r;
+        endif
+    endfor
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % plot solutions                             %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    hold on
+    plot(x, uminmod, 'r.-'); 
+    plot(x, usuperbee, 'b.-'); 
+    plot(x, uvanleer, 'g.-'); 
+    plot(x, uupwind, 'c.-'); 
+    plot(x, uexact, 'k.-'); 
+    axis([a,b,u_r-0.1,u_l+0.1])
+    drawnow
+    clf;
+endfor
+input("Press any key to continue.");
