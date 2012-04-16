e62d0cda7bda8ddca6b9e100739e6dbd779dbc5d
Marty Fuhry <martyfuhry@gmail.com>
2012-04-16 15:40:48 -0400

    finished as fuck

--- final/problem1b.m
+++ final/problem1b.m
@@ -15,12 +15,11 @@ b = 1;
 timesteps = 1000;
 
 dx = (b - a) / (m - 1);    % space discretization
-dt = 0.9*dx;
+dt = 1.0*dx;
 x  = [a:dx:b]';      % grid
 
-% tridiagaonal matrix with [1 -2 1] 
-A = 3*diag(ones(m,1)) - 4*diag(ones(m-1,1),-1) + diag(ones(m-2,1),2);
-B = 1*diag(ones(m,1)) - 2*diag(ones(m-1,1),-1) + diag(ones(m-2,1),2);
+A = 3*diag(ones(m,1)) - 4*diag(ones(m-1,1),-1) + diag(ones(m-2,1),-2);
+B = 1*diag(ones(m,1)) - 2*diag(ones(m-1,1),-1) + diag(ones(m-2,1),-2);
 
 mu = alpha * dt / dx;
 
@@ -29,7 +28,6 @@ C = eye(m,m) + -mu/2 * A + mu^2/2 * B;
 U = sech(10*(x - 0.5));
 
 for t = 1:timesteps        % loop through the timesteps
-    U(1) = 0;
     U = C*U;
     plot(x,U);             % plot the result
     axis([a,b,0,1])
